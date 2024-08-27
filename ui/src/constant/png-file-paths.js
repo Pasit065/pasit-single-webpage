7ff099ae947adf5a398e4c4e9123c55b1459a6a9
Pasit065 <pan289277@gmail.com>
2024-08-27 17:48:18 +0700

    Add png-file-path and url-storage constant module

---
+++ ui/src/constant/png-file-paths.js
@@ -0,0 +1,8 @@
+class PngFilePaths {
+    constructor(pngParrentPath) {
+        this.FACEBOOK_LOGO = pngParrentPath + "github-logo.png";
+        this.YOUTUBE_LOGO  = pngParrentPath + "youtube-logo.png";
+        this.GITHUB_LOGO  = pngParrentPath + "github-logo.png";
+        this.INSTRAGRAM_LOGO  = pngParrentPath + "instragram-logo.png";
+    }
+}
---
+++ ui/src/constant/url-storage.js
@@ -0,0 +1,10 @@
+class UrlStorage {
+    constructor() {
+        this.FACEBOOK_URL = "http://facebook.com";
+        this.GITHUB_URL = "https://github.com/Pasit065";
+        this.YOUTUBE_URL = "http://youtube.com";
+        this.INSTRAGAM = "http://instragram.com";
+    }
+}
+
+export default UrlStorage;
